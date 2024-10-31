af94179527349f21956c5fbb628b6db0204772ac
Abboskin <abboskinsenpai@gmail.com>
2024-11-01 02:58:53 +0500

    Homework_4 was done

--- Homework_4/temperature.py
+++ Homework_4/temperature.py
@@ -1,13 +1,10 @@
-def change_c_to_f(c):
+def convert_cel_to_far(c):
     return c * 9 / 5 + 32
-
-def change_f_to_c(f):
+def convert_far_to_cel(f):
     return (f - 32) * 5 / 9
-
 f = float(input("Enter a temperature in degrees F: "))
-c = change_f_to_c(f)
+c = convert_far_to_cel(f)
 print(f"{f} degrees F = {round(c, 2)} degrees C")
-
 c = float(input("Enter a temperature in degrees C: "))
-f = change_c_to_f(c)
+f = convert_cel_to_far(c)
 print(f"{c} degrees C = {round(f, 2)} degrees F")
